837f2b850889b8026812cce00eedd745bf062dc8
Miksuu <imiksuu96@gmail.com>
2023-07-24 03:39:05 +0300

    Ammo reloading test cpp file

---
+++ Missions/[55-2hc]warfarev2_073v48co.chernarus/config.cpp
@@ -0,0 +1,34 @@
+class CfgPatches {
+    // Other CfgPatches entries if you have any
+};
+
+class CfgMagazines {
+    class Default; // This is important in Arma 2!
+
+    class 4Rnd_Ch29 : Default // Use Default here
+    {
+        scope = 2;
+        displayName = "Ch-29";
+        ammo = "M_Ch29_AT";
+        count = 2; // Limit the count to 2
+        type = 256; // Vehicle magazine type
+        initSpeed = 0;
+        sound[] = {
+            "\ca\Weapons\Data\Sound\TOW_2",
+            3.16228,
+            1,
+            1400
+        };
+        reloadMagazineSound[] = {
+            "",
+            0.000316228,
+            1
+        };
+        //canReload = 0; // Disallow reloading
+    };
+
+    class 6Rnd_Ch29 : 4Rnd_Ch29
+    {
+        count = 2;
+    };
+};
