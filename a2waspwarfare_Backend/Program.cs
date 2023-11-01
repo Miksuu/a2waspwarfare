0089859d46025355e96a2a2e61c196c57e8e093c
Miksuu <imiksuu96@gmail.com>
2023-11-01 11:35:27 +0200

    Add HelloWorld program with the gitignore

---
+++ a2waspwarfare_Backend/Program.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello World");
+    }
+    //static void Main() => InsertMethodHere();
+}
