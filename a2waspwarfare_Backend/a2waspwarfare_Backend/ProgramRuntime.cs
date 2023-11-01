15a9d4221810d0c0ee5cb25d12030b59471f1c28
Miksuu <imiksuu96@gmail.com>
2023-11-01 12:19:19 +0200

    Cache the token instead of using it as a parameter

--- a2waspwarfare_Backend/a2waspwarfare_Backend/ProgramRuntime.cs
+++ a2waspwarfare_Backend/a2waspwarfare_Backend/ProgramRuntime.cs
@@ -11,19 +11,20 @@ class ProgramRuntime
     private static readonly HttpClient client = new HttpClient();
     private static string backendAlias = "Skynet";
     private static ulong serverId = 19801218;
+    private static string token;
 
     public static async Task RunBackend()
     {
+        token = await File.ReadAllTextAsync("token.txt");
         await testMethod();
     }
 
     public static async Task testMethod()
     {
-        string token = await File.ReadAllTextAsync("token.txt");
-        await SendMessage(token, "Mr.James", $"{backendAlias}: hello james, this is {backendAlias} speaking");
+        await SendMessage("Mr.James", $"{backendAlias}: hello james, this is {backendAlias} speaking");
     }
 
-    private static async Task SendMessage(string _token, string _playerName, string _message)
+    private static async Task SendMessage(string _playerName, string _message)
     {
         try
         {
@@ -46,7 +47,7 @@ class ProgramRuntime
             string json = JsonConvert.SerializeObject(command);
             var content = new StringContent(json, Encoding.UTF8, "application/json");
 
-            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", _token);
+            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
             var response = await client.PostAsync($"https://api.battlemetrics.com/servers/{serverId}/rcon", content);
 
             response.EnsureSuccessStatusCode();
