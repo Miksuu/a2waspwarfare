6cc8e2bb95cb4faa28ebb0a895eeb2112bc8ff7c
Miksuu <imiksuu96@gmail.com>
2023-11-24 10:15:53 +0200

    Insert verbose logging

--- Tools/LoadoutManager/ZipManager.cs
+++ Tools/LoadoutManager/ZipManager.cs
@@ -8,7 +8,13 @@ public class ZipManager
     {
         string a2waspDirectory = FileManager.FindA2WaspWarfareDirectory().FullName;
         string[] missionDirectories = { "Missions", "Missions_Vanilla", "Modded_Missions" };
+        // Create this directory if it doesn't exist
         string tempDirectory = @"TempZippingDirectory";
+        if (!Directory.Exists(tempDirectory))
+        {
+            CreateDirectory(tempDirectory);
+        }
+        
         string destinationFile = @"_MISSIONS.zip";
 
         CreateDirectory(tempDirectory);
@@ -28,18 +34,21 @@ public class ZipManager
     private static void CreateDirectory(string _directoryPath)
     {
         Directory.CreateDirectory(_directoryPath);
+        Console.WriteLine($"Created directory: {_directoryPath}");
     }
 
     // This method deletes a directory
     private static void DeleteDirectory(string _directoryPath)
     {
         Directory.Delete(_directoryPath, true);
+        Console.WriteLine($"Deleted directory: {_directoryPath}");
     }
 
     // This method creates a zip file from a directory
     private static void CreateZipFromDirectory(string _sourceDirectory, string _destinationFile)
     {
         ZipFile.CreateFromDirectory(_sourceDirectory, _destinationFile);
+        Console.WriteLine($"Created zip file: {_destinationFile}");
     }
 
     // This method copies files from one directory to another, ignoring symlinks
@@ -57,6 +66,7 @@ public class ZipManager
             if (!IsSymlink(file))
             {
                 File.Copy(file, Path.Combine(_destinationDirectory, Path.GetFileName(file)));
+                Console.WriteLine($"Copied file: {file} to {_destinationDirectory}");
             }
         }
     }
@@ -65,6 +75,11 @@ public class ZipManager
     private static bool IsSymlink(string _path)
     {
         var fileAttributes = File.GetAttributes(_path);
-        return fileAttributes.HasFlag(FileAttributes.ReparsePoint);
+        bool isSymlink = fileAttributes.HasFlag(FileAttributes.ReparsePoint);
+        if (isSymlink)
+        {
+            Console.WriteLine($"File {_path} is a symlink");
+        }
+        return isSymlink;
     }
 }
