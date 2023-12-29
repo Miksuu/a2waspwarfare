26fe4d6855019940fbe3bb134c65665a0fcbf521
Miksuu <imiksuu96@gmail.com>
2023-12-29 12:55:42 +0200

    Add RESTARTSERVER extension call (non functional yet)

---
+++ a2waspwarfare_Extension/a2waspwarfare_Extension/BaseExtensionClass/Implementations/RESTARTSERVER.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class RESTARTSERVER : BaseExtensionClass
+{
+    public override void ActivateExtensionMethodOnTheDerivedClass(string[] _args)
+    {
+        try
+        {
+            Log.WriteLine("Restarting server", LogLevel.DEBUG);
+        
+            
+
+            Log.WriteLine("Done restarting server", LogLevel.DEBUG);
+        }
+        catch (Exception _ex)
+        {
+            Log.WriteLine(_ex.Message, LogLevel.CRITICAL);
+            throw new InvalidOperationException(_ex.Message);
+        }
+    }
+}
