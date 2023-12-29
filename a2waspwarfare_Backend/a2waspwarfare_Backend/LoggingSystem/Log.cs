8b971439f458153b0253784a00af71ad5c33561a
Miksuu <imiksuu96@gmail.com>
2023-12-29 15:50:12 +0200

    Add custom path to the backend logging

--- a2waspwarfare_Backend/a2waspwarfare_Backend/LoggingSystem/Log.cs
+++ a2waspwarfare_Backend/a2waspwarfare_Backend/LoggingSystem/Log.cs
@@ -5,7 +5,7 @@ using System.Runtime.CompilerServices;
 
 public static class Log
 {
-    public static string logsPath = @"C:\a2waspwarfare\Logs\";
+    public static string logsPath = @"C:\a2waspwarfare_Backend\Logs\";
 
     public static void WriteLine(
         string _message,
