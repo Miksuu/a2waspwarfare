41f30af5b64eb8d22cac2da686ffe7814271eedd
Miksuu <imiksuu96@gmail.com>
2023-12-29 18:51:28 +0200

    Add better logging

--- a2waspwarfare_Backend/a2waspwarfare_Backend/ServerManager.cs
+++ a2waspwarfare_Backend/a2waspwarfare_Backend/ServerManager.cs
@@ -6,23 +6,31 @@ public static class ServerManager
 {
     public static async Task RestartServerAndHeadlessClient()
     {
-        Log.WriteLine("Restarting server");
+        Log.WriteLine("Initiating server restart process", LogLevel.VERBOSE);
 
         // Restart the service named A2OAServer
         ServiceController service = new ServiceController("A2OAServer");
         if (service.Status != ServiceControllerStatus.Stopped)
         {
+            Log.WriteLine("Stopping the A2OAServer service", LogLevel.VERBOSE);
             service.Stop();
             service.WaitForStatus(ServiceControllerStatus.Stopped);
+            Log.WriteLine("A2OAServer service stopped successfully", LogLevel.VERBOSE);
         }
 
+        Log.WriteLine("Modifying rotation", LogLevel.VERBOSE);
         await RotationManager.ModifyRotation();
+        Log.WriteLine("Rotation modified successfully", LogLevel.VERBOSE);
 
+        Log.WriteLine("Rebooting headless client and setting it to real time priority", LogLevel.VERBOSE);
         Task.Run(() => HeadlessClientManager.RebootHeadlessClientAndSetItToRealTimePriority());
+        Log.WriteLine("Headless client rebooted and set to real time priority successfully", LogLevel.VERBOSE);
         
+        Log.WriteLine("Starting the A2OAServer service", LogLevel.VERBOSE);
         service.Start();
         service.WaitForStatus(ServiceControllerStatus.Running);
+        Log.WriteLine("A2OAServer service started successfully", LogLevel.VERBOSE);
 
-        Log.WriteLine("Done restarting server, killing headless client.");
+        Log.WriteLine("Server restart process completed", LogLevel.VERBOSE);
     }
 }
