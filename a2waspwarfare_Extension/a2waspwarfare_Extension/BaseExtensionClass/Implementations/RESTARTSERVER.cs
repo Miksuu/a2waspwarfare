119c7410784191cb7d75ff2fef338c576a6adc82
Miksuu <imiksuu96@gmail.com>
2023-12-29 16:21:32 +0200

    Create a new process instead of running it in the extension to avoid the shutdown of the backend.

--- a2waspwarfare_Extension/a2waspwarfare_Extension/BaseExtensionClass/Implementations/RESTARTSERVER.cs
+++ a2waspwarfare_Extension/a2waspwarfare_Extension/BaseExtensionClass/Implementations/RESTARTSERVER.cs
@@ -14,12 +14,18 @@ public class RESTARTSERVER : BaseExtensionClass
             Process[] pname = Process.GetProcessesByName("a2waspwarfare_Backend");
             if (pname.Length == 0)
             {
-                Process.Start(new ProcessStartInfo
+                // Start a new process that runs a separate application to handle the backend
+                ProcessStartInfo startInfo = new ProcessStartInfo
                 {
                     FileName = "a2waspwarfare_Backend.exe",
                     WorkingDirectory = @"C:\a2waspwarfare_Backend",
-                    Arguments = "true"
-                });
+                    Arguments = "true",
+                    UseShellExecute = false, // Don't use operating system shell to start the process
+                    CreateNoWindow = true, // Don't create a new window
+                };
+
+                Process backendProcess = new Process { StartInfo = startInfo };
+                backendProcess.Start();
             }
             else
             {
