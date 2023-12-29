1f403341caef60287eaeecf2a02a2846789caf5d
Miksuu <imiksuu96@gmail.com>
2023-12-29 13:05:09 +0200

    Split the BM API stuff to another file (not used for now)

--- a2waspwarfare_Backend/a2waspwarfare_Backend/ProgramRuntime.cs
+++ a2waspwarfare_Backend/a2waspwarfare_Backend/ProgramRuntime.cs
@@ -8,72 +8,8 @@ using Newtonsoft.Json;
 
 class ProgramRuntime
 {
-    private static readonly HttpClient client = new HttpClient();
-    private static string backendAlias = "Skynet";
-    private static ulong serverId = 19801218;
-    private static string? token;
-    private static string? sessionId;
-
     public static async Task RunBackend()
     {
-        token = await File.ReadAllTextAsync("token.txt");
-        sessionId = await File.ReadAllTextAsync("sessionId.txt");
-        await testMethod();
-    }
 
-    public static async Task testMethod()
-    {
-        await SendMessage("Mr.James", $"{backendAlias}: hello james, this is {backendAlias} speaking");
-    }
-
-    private static async Task SendMessage(string _playerName, string _message)
-    {
-        try
-        {
-            var command = new
-            {
-                data = new
-                {
-                    type = "rconCommand",
-                    attributes = new
-                    {
-                        command = "arma:privateChat",
-                        options = new
-                        {
-                            message = $"{_message}"
-                        }
-                    },
-                    relationships = new
-                    {
-                        session = new
-                        {
-                            data = new
-                            {
-                                type = "session",
-                                id = sessionId
-                            }
-                        }
-                    }
-                }
-            };
-
-            string json = JsonConvert.SerializeObject(command);
-            var content = new StringContent(json, Encoding.UTF8, "application/json");
-
-            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", token);
-            var response = await client.PostAsync($"https://api.battlemetrics.com/servers/{serverId}/command", content);
-
-            if (!response.IsSuccessStatusCode)
-            {
-                string errorContent = await response.Content.ReadAsStringAsync();
-                Console.WriteLine($"Error: {errorContent}");
-            }
-
-            response.EnsureSuccessStatusCode();
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine($"An error occurred: {ex.Message}");
-        }
     }
 }
