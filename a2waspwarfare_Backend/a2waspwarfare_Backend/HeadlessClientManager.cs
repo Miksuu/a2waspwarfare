856148d8b4141b09e9eb71e9999a1ae5a27fa2c2
Miksuu <imiksuu96@gmail.com>
2023-12-29 18:58:26 +0200

    Set logging

--- a2waspwarfare_Backend/a2waspwarfare_Backend/HeadlessClientManager.cs
+++ a2waspwarfare_Backend/a2waspwarfare_Backend/HeadlessClientManager.cs
@@ -6,19 +6,25 @@ public static class HeadlessClientManager
 {
     public static async Task RebootHeadlessClientAndSetItToRealTimePriority()
     {
+        Log.WriteLine("Initiating reboot of headless client and setting process to real time priority", LogLevel.VERBOSE);
+
         // Wait for 2 minutes before killing the headless client and setting the process to real time priority
         await Task.Delay(TimeSpan.FromMinutes(2));
 
         Process[] armaProcesses = Process.GetProcessesByName("Arma2OA");
         foreach (Process armaProcess in armaProcesses)
         {
+            Log.WriteLine("Killing Arma2OA process with ID: " + armaProcess.Id, LogLevel.VERBOSE);
             armaProcess.Kill();
         }
 
         armaProcesses = Process.GetProcessesByName("Arma2OA");
         foreach (Process armaProcess in armaProcesses)
         {
+            Log.WriteLine("Setting Arma2OA process with ID: " + armaProcess.Id + " to real time priority", LogLevel.VERBOSE);
             armaProcess.PriorityClass = ProcessPriorityClass.RealTime;
         }
+
+        Log.WriteLine("Reboot of headless client and setting process to real time priority completed", LogLevel.VERBOSE);
     }
 }
