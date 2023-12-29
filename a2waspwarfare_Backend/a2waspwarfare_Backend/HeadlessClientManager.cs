0a4c425b9f0c547c027df59c5b261fc6b0113059
Miksuu <imiksuu96@gmail.com>
2023-12-29 18:57:42 +0200

    Wait 2 minutes then kill the HC and set the prio

--- a2waspwarfare_Backend/a2waspwarfare_Backend/HeadlessClientManager.cs
+++ a2waspwarfare_Backend/a2waspwarfare_Backend/HeadlessClientManager.cs
@@ -6,14 +6,14 @@ public static class HeadlessClientManager
 {
     public static async Task RebootHeadlessClientAndSetItToRealTimePriority()
     {
+        // Wait for 2 minutes before killing the headless client and setting the process to real time priority
+        await Task.Delay(TimeSpan.FromMinutes(2));
+
         Process[] armaProcesses = Process.GetProcessesByName("Arma2OA");
         foreach (Process armaProcess in armaProcesses)
         {
             armaProcess.Kill();
         }
-
-        // Wait for 2 minutes before setting the process to real time priority
-        await Task.Delay(TimeSpan.FromMinutes(2));
 
         armaProcesses = Process.GetProcessesByName("Arma2OA");
         foreach (Process armaProcess in armaProcesses)
