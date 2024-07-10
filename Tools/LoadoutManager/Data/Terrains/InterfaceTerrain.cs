812e9596b0efe32afa3dbcdb759587ad48fac7f6
Miksuu <imiksuu96@gmail.com>
2024-07-10 14:00:03 +0300

    Remove Tasmania completely from the files (bugged, unusable map), causing error upon any mission load with a clean repo with due to version.sqf gitignore
    was definitely quality work by a senior software engineer whos regex scripts break the whole mission

--- Tools/LoadoutManager/Data/Terrains/InterfaceTerrain.cs
+++ Tools/LoadoutManager/Data/Terrains/InterfaceTerrain.cs
@@ -8,9 +8,6 @@
     public bool isNavalTerrain { get; set; }
     public string inGameMapName { get; set; }
     
-    // Could be removed after Tasmania is fixed
-    public bool skipProcessing { get; set; }
-    
     public void WriteAndUpdateTerrainFiles(
         string _easaFileString, string _commonBalanceFileString, string _aircraftDisplayNameStrings, string _addedAircraftDamageModelChanges, string _coreModFile = "");
 
