8cafa5b05c85ace395887a45817416892e915317
Miksuu <imiksuu96@gmail.com>
2025-07-10 18:14:06 +0300

    Fix nullable warnings in the logEnum file

--- a2waspwarfare_DiscordBot/a2waspwarfare_DiscordBotScripts/LoggingSystem/CustomVariables/logEnum.cs
+++ a2waspwarfare_DiscordBot/a2waspwarfare_DiscordBotScripts/LoggingSystem/CustomVariables/logEnum.cs
@@ -3,24 +3,30 @@ using System.Runtime.Serialization;
 using System.Text;
 
 [DataContract]
-public class logEnum<T> where T : Enum
+public class logEnum<T> where T : struct, Enum
 {
     [DataMember]
-    private T value;
+    private T? _value = null;
 
     public logEnum() : base()
     {
-        value = default(T);
+        // _value is already initialized to null
     }
 
-    public T GetValue(
+    public logEnum(T value)
+    {
+        Log.WriteLine("Creating " + nameof(logEnum<T>) + " with value: " + value, LogLevel.SET_VERBOSE);
+        _value = value;
+    }
+
+    public T? GetValue(
         [CallerFilePath] string _filePath = "",
         [CallerMemberName] string _memberName = "",
         [CallerLineNumber] int _lineNumber = 0)
     {
         Log.WriteLine("Getting " + _memberName,
                      LogLevel.GET_VERBOSE, _filePath, "", _lineNumber);
-        return value;
+        return _value;
     }
 
     public void SetValue(T value,
@@ -30,6 +36,6 @@ public class logEnum<T> where T : Enum
     {
         Log.WriteLine("Setting " + _memberName +
             " TO: " + value, LogLevel.SET_VERBOSE, _filePath, "", _lineNumber);
-        this.value = value;
+        _value = value;
     }
 }
