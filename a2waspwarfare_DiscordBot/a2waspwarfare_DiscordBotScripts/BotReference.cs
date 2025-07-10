8aeb24d48b8cea7a7f3d4c2ebc0a841db36f5455
Miksuu <imiksuu96@gmail.com>
2025-07-10 18:22:14 +0300

    Add the BotReference file

---
+++ a2waspwarfare_DiscordBot/a2waspwarfare_DiscordBotScripts/BotReference.cs
@@ -0,0 +1,71 @@
+﻿using Discord;
+using Discord.WebSocket;
+
+// Reference to the bot's client/guild reference variable
+public class BotReference
+{
+    public bool ConnectionState
+    {
+        get => connectionState.GetValue();
+        set => connectionState.SetValue(value);
+    }
+
+    private static BotReference? instance;
+    private static readonly object lockObject = new object();
+    private static DiscordSocketClient? clientRef;
+    private static SocketGuild? guildRef;
+    private logVar<bool> connectionState = new logVar<bool>();
+
+    private BotReference() { }
+
+    public static BotReference Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                lock (lockObject)
+                {
+                    if (instance == null)
+                    {
+                        Log.WriteLine("Creating a new instance for " + nameof(BotReference), LogLevel.DEBUG);
+                        instance = new BotReference();
+                    }
+                }
+            }
+            return instance;
+        }
+    }
+
+    public static DiscordSocketClient GetClientRef()
+    {
+        if (clientRef == null)
+        {
+            string errorMessage = "ClientRef was null!";
+            Log.WriteLine(errorMessage, LogLevel.ERROR);
+            throw new InvalidOperationException(errorMessage);
+        }
+        return clientRef;
+    }
+
+    public static DiscordSocketClient SetClientRefAndReturnIt()
+    {
+        var config = new DiscordSocketConfig()
+        {
+            GatewayIntents = GatewayIntents.All
+        };
+
+        clientRef = new DiscordSocketClient(config);
+        return clientRef;
+    }
+
+    public static SocketGuild GetGuildRef()
+    {
+        if (guildRef == null)
+        {
+            guildRef = clientRef.GetGuild(Preferences.Instance.GuildID);
+        }
+
+        return guildRef;
+    }
+}
