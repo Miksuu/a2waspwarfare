531c146b67a54bfd506d96782186db709f27b46e
Miksuu <imiksuu96@gmail.com>
2025-07-10 18:20:11 +0300

    Remove the database list from the SerializationManager

--- a2waspwarfare_DiscordBot/a2waspwarfare_DiscordBotScripts/SerializationManager.cs
+++ a2waspwarfare_DiscordBot/a2waspwarfare_DiscordBotScripts/SerializationManager.cs
@@ -8,8 +8,6 @@ public static class SerializationManager
 {
     static SemaphoreSlim semaphore = new SemaphoreSlim(1);
 
-    public static List<Database> listOfDatabaseInstances =
-        new List<Database>() { Database.GetInstance<DiscordBotDatabase>(), Database.GetInstance<ApplicationDatabase>() };
     public static async Task SerializeDB()
     {
         await semaphore.WaitAsync();
