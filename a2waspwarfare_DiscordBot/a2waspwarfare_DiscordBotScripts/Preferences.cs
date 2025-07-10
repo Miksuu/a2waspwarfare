3010ecd970481c73c55a19f232ffc5b2f1ae60e5
Miksuu <imiksuu96@gmail.com>
2025-07-10 18:26:35 +0300

    Add the preferences singleton file

---
+++ a2waspwarfare_DiscordBot/a2waspwarfare_DiscordBotScripts/Preferences.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+
+public sealed class Preferences
+{
+    // Singleton stuff
+    private static Preferences? instance;
+    private static readonly object padlock = new object();
+
+    public ulong GuildID;
+
+    public static Preferences Instance
+    {
+        get
+        {
+            lock (padlock)
+            {
+                if (instance == null)
+                {
+                    string json = File.ReadAllText("preferences.json");
+                    instance = JsonConvert.DeserializeObject<Preferences>(json);
+                }
+            }
+#pragma warning disable CS8603 // Possible null reference return.
+            return instance;
+#pragma warning restore CS8603 // Possible null reference return.
+        }
+        set
+        {
+            instance = value;
+        }
+    }
+}
